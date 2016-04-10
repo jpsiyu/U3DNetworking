fc0bb937522c77e25ab4888435230ba8169e5fd8
jpsiyu <jpsiyu@163.com>
2016-04-10 22:55:28 +0800

    failed connect and i don't know how to do

--- Assets/Scripts/NetworkingView.cs
+++ Assets/Scripts/NetworkingView.cs
@@ -3,13 +3,24 @@ using System.Collections;
 
 public class NetworkingView : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private string connectionIP = "127.0.0.1";
+    private int connectionPort = 25001;
+
+    void OnGUI() {
+        if (Network.peerType == NetworkPeerType.Disconnected) {
+            GUI.Label(new Rect(10, 10, 200, 20), "Status: Disconnected");
+        }
+        if (GUI.Button(new Rect(10, 30, 120, 20), "Client Connect")) {
+            Network.Connect(connectionIP, connectionPort);
+        }
+        if (GUI.Button(new Rect(10, 50, 120, 20), "Initailize Server")) {
+            Network.InitializeServer(32, connectionPort, false);
+        }
+        else if (Network.peerType == NetworkPeerType.Client) {
+            GUI.Label(new Rect(10, 10, 300, 20), "Status:Connected as client");
+            if (GUI.Button(new Rect(10, 30, 120, 20), "Disconnect")) {
+                Network.Disconnect(200);
+            }
+        }
+    }
 }
